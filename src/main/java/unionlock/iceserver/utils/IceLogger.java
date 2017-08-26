f33120141bcfaef056d11b8f864c0984e2489ddc
pc <pc@CS-T-Z0002>
2017-08-26 14:13:37 +0800

    second

--- src/main/java/unionlock/iceserver/utils/IceLogger.java
+++
@@ -1,9 +0,0 @@
-package unionlock.iceserver.utils;
-
-
-public class IceLogger {
-	private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger("ICESERVER");
-	public static void log(String msg) {
-		logger.info(msg);
-	}
-}
